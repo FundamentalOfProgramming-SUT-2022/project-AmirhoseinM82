9e7a85208c10f02aac30add15eb9d354632c9a65
unknown <mirdariyanamirhosein@gmail.com>
2023-01-07 11:43:22 +0330

    createfile added

---
+++ fop_project.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<unistd.h>
+
+
+
+
+int main(){
+
+  FILE *fp;
+
+  char command[20];
+
+  while(1){
+
+    scanf("%s",command);
+
+    if(strcmp(command,"createfile")==0){
+
+         char f[7];
+         scanf("%s",f);
+           getchar();
+             if(strcmp(f,"--file")==0){
+
+                char path[200];
+                char c;
+                   if((c=getchar())=='"'){
+                       getchar();
+                        int i=0;
+
+                        do{
+                           c=getchar();
+                           path[i]=c;
+                           i++;
+                        }while(c!='"');
+
+                         int j=0;
+                         char path1[200];
+
+                            while(path[j]!='"'){
+                                if(path[j]=='/'){
+
+                                    path1[j]='\0';
+                                    mkdir(path1);
+                                }
+                                path1[j]=path[j];
+                                j++;
+                            }
+                            path1[j]='\0';
+                            fp=fopen(path1,"w");
+
+                   }else{
+
+                        int i=0;
+
+                        while((c=getchar())!='\n'){
+                           path[i]=c;
+                           i++;
+                        }
+                        int j=0;
+                         char path1[200];
+
+                            while(path[j]!='"'){
+                                if(path[j]=='/'){
+
+                                    path1[j]='\0';
+                                    mkdir(path1);
+                                }
+                                path1[j]=path[j];
+                                j++;
+                            }
+                            path1[j-1]='\0';
+                            fp=fopen(path1,"w");
+                   }
+
+
+         }else{printf("Invalid command\n");}
+
+    }else
+    if(strcmp(command,"exit")==0){
+        return 0;
+    }
+    else
+    {
+        char a;
+        while((a=getchar())!='\n'){
+        }
+        printf("Invalid command\n");
+    }
+  }
+}
